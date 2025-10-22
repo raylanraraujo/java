c93b64d7976cdd978fe849c5dfe8c6277d9c07b5
Raylan Araujo <raylanra@gmail.com>
2025-10-22 07:40:17 -0300

    inserido métodos de entrada

--- TiposPrimitivos/src/TiposPrimitivos.java
+++ TiposPrimitivos/src/TiposPrimitivos.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class TiposPrimitivos {
     public static void main(String[] args) {
-        String nome = "Raylan";
-        float nota = 9.5f; //float nota = (float) 8.5;
-        System.out.print("A nota é " + nota);
-        System.out.println("A nota é " + nota);
-        System.out.printf("A nota de %s é %.2f.\n", nome, nota); 
-        System.out.format("A nota de %s é %.2f.\n", nome, nota);
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite o nome do aluno: "); //exibe na tela o que é preciso que o usuário digite
+        String nome = teclado.nextLine(); //serve para ler a entrada String do usuario pelo teclado
+        System.out.format("Digite a nota do %s: ", nome); //exibe na tela o que é preciso que o usuário digite
+        float nota = teclado.nextFloat(); //serve para ler a entrada Float do usuario pelo teclado
+        System.out.format("\nA nota de %s é %.2f.\n", nome, nota); //Exibe o resultado final com todas as informações
     }
 }
