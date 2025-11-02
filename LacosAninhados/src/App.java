3beb270639d1122c87b07a715e2e9dd365d96010
Raylan Araujo <raylanra@gmail.com>
2025-11-02 12:03:51 -0300

    criado o arquivo lacos aninhados

---
+++ LacosAninhados/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i < 3; i++) {
+            for(int j = 0; j <= 2; j +=2) {
+                System.out.printf("Valor de i: %d\nValor de j: %d\n\n", i, j);
+            }
+        }
+    }
+}
