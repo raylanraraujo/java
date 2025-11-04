c5d09ee1967a99854f73ad2f6aaaeb37bade6a9c
Raylan Araujo <raylanra@gmail.com>
2025-11-04 07:39:36 -0300

    criado TesteFuncao2

---
+++ TesteFuncao2/src/Operacoes.java
@@ -0,0 +1,11 @@
+
+// método que retorne uma string
+public class Operacoes {
+    public static  String contador(int inicio, int fim){
+        String s = "";
+        for (int c = inicio; c <= fim; c++){
+            s += c + " ";
+        }
+        return s;
+    }
+}
