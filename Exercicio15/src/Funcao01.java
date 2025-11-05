826ec4d363f411cf7b76169be4d255446c73a9c4
Raylan Araujo <raylanra@gmail.com>
2025-11-05 06:45:05 -0300

    finalizado o exercicio15

--- Exercicio15/src/Funcao01.java
+++ Exercicio15/src/Funcao01.java
@@ -1,5 +1,8 @@
 public class Funcao01 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Fatorial f = new Fatorial(); //cria (instancia) um novo objeto da classe Fatorial na memória que é armazenado na variável f
+        f.setValor(5);
+        System.out.print(f.getFormula());
+        System.out.printf("%d \n",f.getFatorial());
     }
 }
