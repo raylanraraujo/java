4e5d7e2bb2c3f4d99f62b5b99a6677822ac89df8
Raylan Araujo <raylanra@gmail.com>
2025-11-05 06:23:18 -0300

    finalizada a classe Fatorial do exercício

--- Exercicio15/src/Fatorial.java
+++ Exercicio15/src/Fatorial.java
@@ -5,11 +5,26 @@ public class Fatorial {
     private int fatorial = 1; // o resultaado do fatorial do número
     private String formula= ""; // uma string com a fórmula
 
+
     public void setValor(int n){
-        numero = n;
-        int f = 1;
-        for (int c = numero; c >= 1; c--){
+        numero = n; //o valor passado como parâmetro será atribuido ao atributo numero
+        
+        int f = 1; //criado para auxiliar no cálculo do fatorial do número
+        String s = "";
+        for (int c = numero; c > 1; c--){ //loop para fazer o calculo do fatorial
             f *= c;
+            s += c + " x ";
         }
+        s += "1 = "; //serve para incluir o valor de 1 no final da fórmula já que pelo loop ele não irá aparecer.
+        fatorial = f;
+        formula = s;
+    }
+
+    public int getFatorial() {
+        return fatorial;
+    }
+
+    public String getFormula(){
+        return formula;
     }
 }
