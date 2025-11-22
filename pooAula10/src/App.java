0ef26e107fe770f68f00ae2fd9e150b7f38b47d3
Raylan Araujo <raylanra@gmail.com>
2025-11-22 11:24:09 -0300

    finalizado a codificação da aula

--- pooAula10/src/App.java
+++ pooAula10/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        //programa principal
+        // Pessoa p1 = new Pessoa();
+        // Aluno p2 = new Aluno();
+        // Professor p3 = new Professor;
+        // Funcionario p4 = new Funcionario();
+
+        // p1.setNome("Pedro");
+        // p2.setNome("Maria");
+        // p3.setNome("Claudio");
+        // p4.setNome("Fabiana");
     }
 }
+
+/*
+ *Em java temos que criar os objetos dizendo o tipo abstrato deles na frente. Por exemplo, antes de p2 temos que colocar Aluno para dier que é desse tipo.
+
+
+ */
