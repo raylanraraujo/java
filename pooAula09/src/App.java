8dba510233cf27dc2eeb3952f1ad6d0d2d314b2c
Raylan Araujo <raylanra@gmail.com>
2025-11-22 20:00:09 -0300

    comentado linhas

--- pooAula09/src/App.java
+++ pooAula09/src/App.java
@@ -3,8 +3,8 @@ public class App {
         Pessoa[] pessoas = new Pessoa[2];
         Livro[] livros = new Livro[3];
 
-        pessoas[0] = new Pessoa("Raylan", 36, "Masculino");
-        pessoas[1]= new Pessoa("Débora", 30, "Feminino");
+        // pessoas[0] = new Pessoa("Raylan", 36, "Masculino");
+        // pessoas[1]= new Pessoa("Débora", 30, "Feminino");
 
         livros[0] = new Livro("A Bússola de Ouro", "Philip Pullman", 416, pessoas[0]);
         livros[1] = new Livro("1984", "George Orwell", 328, pessoas[0]);
