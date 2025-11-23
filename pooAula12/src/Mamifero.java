e94e5e541017875c51f49147ad07c8a95a28d623
Raylan Araujo <raylanra@gmail.com>
2025-11-23 10:19:20 -0300

    criado a classe mamiferos

---
+++ pooAula12/src/Mamifero.java
@@ -0,0 +1,23 @@
+public class Mamifero extends Animal{
+    private String corPelo;
+
+    public void locomover(){
+        System.out.println("Correndo");
+    }
+
+    public void alimentar(){
+        System.out.println("Mamando");
+    }
+
+    public void emitirSom(){
+        System.out.println("Som de mamífero.");
+    }
+
+    public String getCorPelo(){
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo){
+        this.corPelo = corPelo;
+    }
+}
