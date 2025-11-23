74eecac1ca0e13905d5c2393f342c8d5c03718b7
Raylan Araujo <raylanra@gmail.com>
2025-11-23 11:05:34 -0300

    criado a classe cachorro

---
+++ pooAula12/src/Cachorro.java
@@ -0,0 +1,9 @@
+public class Cachorro {
+    public void enterrarOsso(){
+        System.out.println("Enterrando osso");
+    }
+
+    public void abanarRabo(){
+        System.out.println("Abanando rabo.");
+    }
+}
