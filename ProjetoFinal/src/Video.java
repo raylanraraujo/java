9671ad579e823e4003183e0973de8fbfb09853c7
Raylan Araujo <raylanra@gmail.com>
2025-11-23 18:00:01 -0300

    criado a classe video e implementado a interface

---
+++ ProjetoFinal/src/Video.java
@@ -0,0 +1,57 @@
+public class Video implements AcoesVideo{
+    private String titulo;
+    private float avaliacao;
+    private int views;
+    private int curtidas;
+    private boolean reproduzindo;
+
+    
+
+    @Override
+    public void like() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void play() {
+        // TODO Auto-generated method stub
+        
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public float getAvaliacao() {
+        return avaliacao;
+    }
+    public void setAvaliacao(float avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+    public int getViews() {
+        return views;
+    }
+    public void setViews(int views) {
+        this.views = views;
+    }
+    public int getCurtidas() {
+        return curtidas;
+    }
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+    public boolean isReproduzindo() {
+        return reproduzindo;
+    }
+    public void setReproduzindo(boolean reproduzindo) {
+        this.reproduzindo = reproduzindo;
+    }
+
+    
+}
