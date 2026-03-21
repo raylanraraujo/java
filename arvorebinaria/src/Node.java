fb13a008404744544907abcea094442157333651
Raylan Araujo <raylanra@gmail.com>
2026-03-21 14:24:51 -0300

    criado a classe Node

---
+++ arvorebinaria/src/Node.java
@@ -0,0 +1,51 @@
+public class Node {
+    private Integer valor;
+    private Node nodeEsquerdo;
+    private Node nodeDireito;
+
+    public Node () {}       //construtor vazio
+
+
+    public Node (Integer valor){ //outro construtor com atribuição de valor
+        super();
+        this.valor = valor;
+    }
+
+
+    public Integer getValor() {
+        return valor;
+    }
+
+
+    public void setValor(Integer valor) {
+        this.valor = valor;
+    }
+
+
+    public Node getNodeEsquerdo() {
+        return nodeEsquerdo;
+    }
+
+
+    public void setNodeEsquerdo(Node nodeEsquerdo) {
+        this.nodeEsquerdo = nodeEsquerdo;
+    }
+
+
+    public Node getNodeDireito() {
+        return nodeDireito;
+    }
+
+
+    public void setNodeDireito(Node nodeDireito) {
+        this.nodeDireito = nodeDireito;
+    }
+
+
+    @Override
+    public String toString() {
+        return "No [valor = " + valor + " ]";
+    }
+
+    
+}
